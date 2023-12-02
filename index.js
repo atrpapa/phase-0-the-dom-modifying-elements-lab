f0d71e7794be0f2f6be159fd1fa3f9b77c7e8bd8
Anthony Ray <rayanthony42@gmail.com>
2023-12-01 19:19:17 -0500

    Geez

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const m = document.getElementById('main');
+m.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Anthony is the champion";
+
+document.body.append("newHeader");
